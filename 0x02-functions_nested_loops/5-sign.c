31de3d1befeca74c1844f5ca07c10b37594491a3
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-13 06:49:56 -0700

    Complete task 5 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_sign - Print sign of a number
+  * @n: The number to retrieve the sign
+  *
+  * Return: 1 if n is greater than 0,
+  * 0 if n is 0,
+  * -1 for n less than 0.
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+}
