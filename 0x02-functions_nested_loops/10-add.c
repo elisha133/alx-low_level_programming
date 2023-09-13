7b360b2bae7b33204fb2470681add18d7ba9f7a0
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-13 14:52:44 -0700

    Complete task 10 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - Add two integers
+  * @n: The first integer
+  * @m: The second integer
+  *
+  * Return: The sum of the integers.
+  */
+int add(int n, int m)
+{
+	return ((n) + (m));
+}
