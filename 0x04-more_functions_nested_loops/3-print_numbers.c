e6f73bab3219a9c55d1da5058dd253305683695a
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-16 03:11:14 -0700

    Complete task 3 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * print_numbers - Print numbers from 0 to 9
+  *
+  * Return: Nothing.
+  */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+		_putchar(c);
+
+	_putchar(10);
+}
