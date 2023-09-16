371950a373fade7c11ff2293e1c39b7f079533e9
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-16 03:58:18 -0700

    Complete task 5 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * more_numbers - Print 10 times the numbers, from 0 to 14
+  *
+  * Return: Nothing.
+  */
+void more_numbers(void)
+{
+	int i, c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+				_putchar(49);
+
+			_putchar((c % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
