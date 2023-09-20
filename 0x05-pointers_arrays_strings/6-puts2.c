8afba06b2dc79dec355fcd9d51ebb6f72bc456cd
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-20 16:21:23 -0700

    Complete task 6 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * puts2 - Print every other character of a string,
+  * starting with the first character
+  * @str: The string to print
+  *
+  * Return: Nothing.
+  */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (i != '\0')
+		i++;
+
+	j = 0;
+	while (j <= (i - 1))
+	{
+		if (j % 2 == 0)
+			_putchar(*(s + j));
+
+		j++;
+	}
+}
