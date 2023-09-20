d4b3bef73f68976d888484b7de4b95c5efff2d7d
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-20 16:25:14 -0700

    Complete task 6 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,14 +12,14 @@ void puts2(char *str)
 	int i, j;
 
 	i = 0;
-	while (i != '\0')
+	while (*(str + i) != '\0')
 		i++;
 
 	j = 0;
 	while (j <= (i - 1))
 	{
 		if (j % 2 == 0)
-			_putchar(*(s + j));
+			_putchar(*(str + j));
 
 		j++;
 	}
