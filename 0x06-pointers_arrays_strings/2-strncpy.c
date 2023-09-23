a73bafe4468dfe37058480a9b05001357d038e78
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-23 16:06:25 -0700

    Complete task 2 2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * _strncpy - Copy a string
+  * @dest: The first string
+  * @src: The second string
+  * @n: Number of charavters to copy
+  *
+  * Return: Pointer to dest.
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	while ()
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+
+	return (dest);
+}
