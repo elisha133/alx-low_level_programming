39449fcfc01599085d7310e24c7c04639e384fa4
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-23 17:34:39 -0700

    Complete task 8 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#included "main.h"
+
+/**
+  * rot13 - Encode a string using rot13
+  * @str: The string
+  *
+  * Return: Point to str.
+  */
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(str + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(str + i))
+			{
+				*(str + i) = b[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
