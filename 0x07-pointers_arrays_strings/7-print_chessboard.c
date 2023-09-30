6f4226377cacef129baaaf3fb6b5b3a15d2cfd37
Elisha Benjamin <benjaminelisha133@gmail.com>
2023-09-29 18:38:39 -0700

    Complete task 7 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_chessboard - Print chessboard
+  * @a: Pointer to the chessboard
+  *
+  * Return: Nothing.
+  */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar(10);
+	}
+}
